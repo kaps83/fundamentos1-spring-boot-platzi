b905a7e697cb6a2632e6562bd199bef2423ae251
enrique perez <kike@MacBook-Air-de-enrique.local>
2022-03-23 11:24:25 -0300

    Âprimer commit

---
+++ src/main/java/com/fundamentos1platzi/springboot/fundamentos1/Fundamentos1Application.java
@@ -0,0 +1,13 @@
+package com.fundamentos1platzi.springboot.fundamentos1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fundamentos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fundamentos1Application.class, args);
+	}
+
+}
